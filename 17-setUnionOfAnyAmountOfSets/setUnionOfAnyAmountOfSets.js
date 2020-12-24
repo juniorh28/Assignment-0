98658e26fab13f1babc752b0c12caba3de75876b
juniorh28 <juniorh2897@gmail.com>
2020-12-23 19:41:26 -0500

    takes a list of sets, merge the set into one set of sets, then makes a set that has the value once and skips any repeating values.

--- 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
+++ 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
@@ -1,6 +1,19 @@
 function setUnionOfAnyAmountOfSets(...args) {
   //this is test-17
+  //given a list of an array of numbers, make a set object that takes them
 
+  //creat an empty set to be added unqiue values
+  let set1 = new Set();
+
+  //take the values of the args and make them into unqiue array
+  for (const value of args) {
+    //take those unique set and add every value once.
+    //ignore repeating values.
+    for (let value2 of value) {
+      set1.add(value2);
+    }
+  }
+  return set1;
 }
 
 // Do not edit this line;
